e241a4504c528020603ec90f176d5d3cc88dcab7
Robert Lin <Rlin415@gmail.com>
2015-12-16 16:52:27 -0800

    load index.html to client

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,28 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require("./http-helpers");
+var fs = require("fs");
 // require more modules/folders here!
 
+var actions = {
+  "GET": function(req, res) {
+    res.writeHead(httpHelper.headers);
+    fs.readFile("./public/index.html", function(err, content) {
+      res.write(content);
+      res.end("Hello, World");
+    });    
+  },
+  "POST": function(req, res) {
+    res.writeHead(httpHelper.headers);
+    res.end(200);
+  },
+  "OPTIONS": function(req, res) {
+    res.writeHead(httpHelper.headers);
+    res.end(200);
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  action(req, res);
 };
